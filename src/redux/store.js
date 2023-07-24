98fa85d0d8464629fb85c868d63f40b9eafdc4c0
moFuhidy <mo7ammed9290@gmail.com>
2023-07-24 16:33:02 +0300

    Fix linter error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketsReducer from './rockets/rocketsSlice';
+import missionsReducer from './missions/missonsSlice';
 
 const store = configureStore(
-  { reducer: rocketsReducer },
+  {
+    reducer: {
+      rockets: rocketsReducer,
+      missions: missionsReducer,
+    },
+  },
   applyMiddleware(logger),
 );
 
