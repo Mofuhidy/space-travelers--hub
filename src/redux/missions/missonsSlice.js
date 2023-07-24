b33fcf54ea5705f09e8626ab8b7d5ecce0f6e8f5
John Murianki <mcjthiongo@gmail.com>
2023-07-25 00:48:56 +0300

    made theselected changes persist even when the status is changed

--- src/redux/missions/missonsSlice.js
+++ src/redux/missions/missonsSlice.js
@@ -3,14 +3,20 @@ import { FetchMissions } from './missionsThunk';
 
 const initialState = {
   missions: [],
+  activeMissions: [], // New array to store active missions
   isLoading: false,
 };
+
 const missionHandlerReducer = (state, { payload }) => ({
   ...state,
   missions: state.missions.map((mission) => (mission.missionId === payload
     ? { ...mission, reserved: !mission.reserved }
     : mission)),
+  activeMissions: state.activeMissions.includes(payload)
+    ? state.activeMissions.filter((missionId) => missionId !== payload)
+    : [...state.activeMissions, payload],
 });
+
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
@@ -30,7 +36,8 @@ const missionsSlice = createSlice({
           missionId: element.missionId,
           missionName: element.missionName,
           description: element.description,
-          reserved: false,
+
+          reserved: state.activeMissions.includes(element.missionId),
         }));
         state.missions = missions;
       })
@@ -39,5 +46,6 @@ const missionsSlice = createSlice({
       });
   },
 });
+
 export const { missionHandler } = missionsSlice.actions;
 export default missionsSlice.reducer;
